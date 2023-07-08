0b4de1c980a88b03960c83c1e3bb65af3b876bfc
MikePowar <mike.powar@gmail.com>
2023-07-07 17:19:10 -0700

    Initial Commit

---
+++ G-Unit Unit Converter/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  G-Unit Unit Converter
+//
+//  Created by Mike Powar on 2023-07-07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ G-Unit Unit Converter/G_Unit_Unit_ConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  G_Unit_Unit_ConverterApp.swift
+//  G-Unit Unit Converter
+//
+//  Created by Mike Powar on 2023-07-07.
+//
+
+import SwiftUI
+
+@main
+struct G_Unit_Unit_ConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
