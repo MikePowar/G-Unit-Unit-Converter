891cc30c61b741f40be2c6747a0f642bc36e409f
MikePowar <mike.powar@gmail.com>
2023-07-12 19:45:36 -0700

    Project outline - plan layout

--- G-Unit Unit Converter/ContentView.swift
+++ G-Unit Unit Converter/ContentView.swift
@@ -5,17 +5,54 @@
 //  Created by Mike Powar on 2023-07-07.
 //
 
+//DAY 19 Hacking with SwiftUI
+
+
+/*
+Challenge:
+1. Users will select an input unit and an output unit
+2. Then Users will enter a value
+3. Program will show users an output of the conversion.
+
+ TIP: So, rather than writing code to convert from liters to milliliters, and from liters to cups, and from liters to pints, and so on, a better idea is to convert the user’s input into milliliters (the lowest common denominator), then convert from there to whatever output unit they want.
+ 
+ - Unit Conversions: convert user input to single base input (LCD)
+- 3 user values = 3 @State properties
+UI:
+ - TextField
+ - 2 pickers
+ - text view for output
+ 
+ Conversions:
+ - store conversion units via String Array
+   that you loop over
+ 
+ */
+
+
+//V1: Lenth Conversion:
+
 import SwiftUI
 
 struct ContentView: View {
+    @State var userInput = 0
+    @State var pickerInput = 0
+    @State var pickerOutput = 0
+    
+    
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, 0.50!")
+            NavigationView {
+                Form {
+                    Image(systemName: "globe")
+                        .imageScale(.large)
+                        .foregroundColor(.accentColor)
+                    Text("Hello, 0.50!")
+                }
+                .navigationTitle("G-unit's Units")
+            }
         }
-        .padding()
     }
 }
 
